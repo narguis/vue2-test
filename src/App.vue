c50317b90affaf9d57761408d8a9756c241d62d7
narguis <narguis.guilherme@gmail.com>
2025-03-05 16:48:03 -0300

    initial styling

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,17 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+}
+
+body {
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
 }
 </style>
